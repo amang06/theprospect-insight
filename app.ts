f10c21902d841a4005d12b9b9761fbe1954530ea
amang06 <aman@skynetiks.com>
2024-03-13 22:39:07 +0530

    stealth

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
-const express = require('express');
-const { chromium, Browser, BrowserContext, Page, Locator } = require('playwright');
-const { selectors, ui } = require('./selectors');
+const express = require("express");
+const { Browser, BrowserContext, Page, Locator } = require("playwright");
+const { selectors, ui } = require("./selectors");
+const { chromium } = require("playwright-extra");
+const stealth = require('puppeteer-extra-plugin-stealth')()
 
 const app = express();
 const port = 3000;
+chromium.use(stealth)
 
-let context: typeof BrowserContext = null; 
+let context: typeof BrowserContext = null;
 
 // Function to initialize browser
 async function initializeBrowser() {
@@ -14,29 +17,28 @@ async function initializeBrowser() {
 }
 
 // Middleware to ensure browser is initialized before processing requests
-app.use(async (req: any, res:any, next: any) => {
+app.use(async (req: any, res: any, next: any) => {
   if (!context) {
     try {
       await initializeBrowser();
-      console.log("Browser initialized successfully")
+      console.log("Browser initialized successfully");
     } catch (error) {
-      console.error('Error initializing browser:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      console.error("Error initializing browser:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
   next();
 });
 
-
-app.get('/', async (req: any, res: any) => {
+app.get("/", async (req: any, res: any) => {
   return res.send("running");
-})
+});
 
-app.get('/url-scrape', async (req: any, res: any) => {
+app.get("/url-scrape", async (req: any, res: any) => {
   try {
     const { url } = req.query; // Get the URL from the query parameters
     if (!url) {
-      return res.status(400).json({ error: 'URL parameter is required' });
+      return res.status(400).json({ error: "URL parameter is required" });
     }
     // const browser = await chromium.launch({ headless: true });
     // const context = await browser.newContext();
@@ -45,24 +47,24 @@ app.get('/url-scrape', async (req: any, res: any) => {
     const google = "https://www.google.com";
 
     await page.goto(google);
-    console.log("Redirecting to Google")
+    console.log("Redirecting to Google");
     await page.waitForSelector(ui.googleSearch);
     await page.locator(ui.googleSearch).fill(url);
-    console.log("Searching for "+url)
+    console.log("Searching for " + url);
     await page.keyboard.press("Enter");
-    console.log("Pressed enter")
+    console.log("Pressed enter");
 
     for (let i = 0; i < 10; i++) {
       await page.waitForSelector(ui.googleSearchResultsPage);
       await page.locator(ui.searchResults).click();
-      console.log("Clicked search results")
+      console.log("Clicked search results");
       await page.waitForTimeout(1000);
       const lpage = await page.locator(selectors.summary).isVisible();
       if (lpage) {
-        console.log("Linkedin page loaded")
+        console.log("Linkedin page loaded");
         break;
       } else {
-        console.log("Linkedin page not loaded, going back")
+        console.log("Linkedin page not loaded, going back");
         await page.goBack();
       }
     }
@@ -74,14 +76,14 @@ app.get('/url-scrape', async (req: any, res: any) => {
     for (const [key, selector] of Object.entries(selectors)) {
       const elements = await page.locator(selector).all();
       const data: Array<string | null> = [];
-  
+
       for (const element of elements) {
         const text = await element.textContent();
-        if(text){
-          data.push(text.replace(/\n/g,"").trim());
+        if (text) {
+          data.push(text.replace(/\n/g, "").trim());
         }
       }
-  
+
       scrapedData[key] = data;
     }
 
@@ -89,26 +91,27 @@ app.get('/url-scrape', async (req: any, res: any) => {
 
     res.json(scrapedData);
   } catch (error) {
-    console.error('Error during scraping: '+ error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error during scraping: " + error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.get('/search-scrape', async (req: any, res: any) => {
+app.get("/search-scrape", async (req: any, res: any) => {
   try {
     const { firstName, lastName, companyName } = req.query; // Get the URL from the query parameters
-   
+
     if (!lastName) {
-      return res.status(400).json({ error: 'lastName parameter is required'});
+      return res.status(400).json({ error: "lastName parameter is required" });
     }
-    if(!firstName)
-    {
-      return res.status(400).json({ error: 'firstName parameter is required'});
+    if (!firstName) {
+      return res.status(400).json({ error: "firstName parameter is required" });
     }
     if (!companyName) {
-      return res.status(400).json({ error: 'companyName parameter is required' });
+      return res
+        .status(400)
+        .json({ error: "companyName parameter is required" });
     }
-      
+
     // const browser = await chromium.launch({ headless: true });
     // const context = await browser.newContext();
     const page = await context.newPage();
@@ -117,7 +120,9 @@ app.get('/search-scrape', async (req: any, res: any) => {
 
     await page.goto(google);
     await page.waitForSelector(ui.googleSearch);
-    await page.locator(ui.googleSearch).fill(firstName+" "+lastName+" "+companyName);
+    await page
+      .locator(ui.googleSearch)
+      .fill(firstName + " " + lastName + " " + companyName);
     await page.keyboard.press("Enter");
 
     for (let i = 0; i < 10; i++) {
@@ -139,14 +144,14 @@ app.get('/search-scrape', async (req: any, res: any) => {
     for (const [key, selector] of Object.entries(selectors)) {
       const elements = await page.locator(selector).all();
       const data: Array<string | null> = [];
-  
+
       for (const element of elements) {
         const text = await element.textContent();
-        if(text){
-          data.push(text.replace(/\n/g,"").trim());
+        if (text) {
+          data.push(text.replace(/\n/g, "").trim());
         }
       }
-  
+
       scrapedData[key] = data;
     }
 
@@ -154,8 +159,8 @@ app.get('/search-scrape', async (req: any, res: any) => {
 
     res.json(scrapedData);
   } catch (error) {
-    console.error('Error during scraping:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error during scraping:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
