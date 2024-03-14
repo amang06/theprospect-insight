eae27c5d35bd883092b19c28c8c0fae8eb6831db
amang06 <aman@skynetiks.com>
2024-03-14 15:27:46 +0530

    bright data

--- app.ts
+++ app.ts
@@ -1,45 +1,32 @@
 const express = require("express");
-const { Browser, BrowserContext, Page, Locator } = require("playwright");
+const {
+  Browser,
+  BrowserContext,
+  Page,
+  Locator,
+  chromium,
+} = require("playwright");
 const { selectors, ui } = require("./selectors");
-const { chromium } = require("playwright-extra");
-const stealth = require("puppeteer-extra-plugin-stealth")();
 
 const app = express();
 const port = 3000;
-chromium.use(stealth);
-
-let context: typeof BrowserContext = null;
-
-// Function to initialize browser
-async function initializeBrowser() {
-  const browser = await chromium.launch({ headless: true });
-  context = await browser.newContext();
-}
-
-// Middleware to ensure browser is initialized before processing requests
-app.use(async (req: any, res: any, next: any) => {
-  if (!context) {
-    try {
-      await initializeBrowser();
-      console.log("Browser initialized successfully");
-    } catch (error) {
-      console.error("Error initializing browser:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
-  next();
-});
 
 app.get("/", async (req: any, res: any) => {
   return res.send("running");
 });
 
 app.get("/url-scrape", async (req: any, res: any) => {
+  const { url } = req.query; // Get the URL from the query parameters
+  if (!url) {
+    return res.status(400).json({ error: "URL parameter is required" });
+  }
+
   try {
-    const { url } = req.query; // Get the URL from the query parameters
-    if (!url) {
-      return res.status(400).json({ error: "URL parameter is required" });
-    }
+    const SBR_CDP =
+      "";
+    const browser = await chromium.connectOverCDP(SBR_CDP);
+
+    const context = await browser.newContext();
 
     const page = await context.newPage();
 
@@ -53,10 +40,11 @@ app.get("/url-scrape", async (req: any, res: any) => {
     await page.keyboard.press("Enter");
     console.log("Pressed enter");
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 2; i++) {
       await page.waitForSelector(ui.googleSearchResultsPage);
+      const result_text = await page.locator(ui.searchResults).textContent();
       await page.locator(ui.searchResults).click();
-      console.log("Clicked search results");
+      console.log("Clicked search results: " + result_text);
       await page.waitForTimeout(1000);
       const lpage = await page.locator(selectors.summary).isVisible();
       if (lpage) {
@@ -64,6 +52,7 @@ app.get("/url-scrape", async (req: any, res: any) => {
         break;
       } else {
         console.log("Linkedin page not loaded, going back");
+        page.screenshot();
         await page.goBack();
       }
     }
@@ -95,6 +84,17 @@ app.get("/url-scrape", async (req: any, res: any) => {
 
 app.get("/search-scrape", async (req: any, res: any) => {
   try {
+    const SBR_WS_ENDPOINT =
+      "";
+    const browser = await chromium.launch({
+      headless: true,
+      proxy: {
+        server: SBR_WS_ENDPOINT,
+        username: "brd-customer-hl_10fb90e3-zone-theprospect_insight",
+        password: "ie4smusfyeor",
+      },
+    });
+    const context = await browser.newContext();
     const { firstName, lastName, companyName } = req.query; // Get the URL from the query parameters
 
     if (!lastName) {
